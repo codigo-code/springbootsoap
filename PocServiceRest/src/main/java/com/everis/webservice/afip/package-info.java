97263915da0ea2b177b57a511189028380b266bd
Dante Panella <dpanella@usersad.everis.int>
2018-04-18 11:49:40 -0300

    update

--- PocServiceRest/src/main/java/com/everis/webservice/afip/package-info.java
+++ PocServiceRest/src/main/java/com/everis/webservice/afip/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2018.04.17 a las 02:14:16 PM ART 
+// Generado el: 2018.04.18 a las 11:49:11 AM ART 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "ar.gov.afip.dia.serviciosweb.wgesTabRef", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
