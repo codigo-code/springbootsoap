aa90d8097610597cdf376794ffcd98a259149a69
pevetni <crseijasnohe@gmail.com>
2018-04-19 16:30:05 -0300

    Consumiendo Servicios Afip SOAP

--- PocServiceRest/src/main/java/com/everis/connectors/SOAPConnector.java
+++ PocServiceRest/src/main/java/com/everis/connectors/SOAPConnector.java
@@ -1,44 +1,11 @@
 package com.everis.connectors;
 
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPException;
-
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class SOAPConnector extends WebServiceGatewaySupport {
 
-	public Object callWebService(String url, Object request) {
-
-		try {
-			SaajSoapMessageFactory messageFactory;
-			messageFactory = new SaajSoapMessageFactory(MessageFactory.newInstance());
-
-			WebServiceTemplate webServiceTemplate = new WebServiceTemplate(messageFactory);
-			messageFactory.afterPropertiesSet();
-			Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-
-			marshaller.setContextPath("com.everis.model");
-			marshaller.afterPropertiesSet();
-
-			webServiceTemplate.setMarshaller(marshaller);
-			webServiceTemplate.setUnmarshaller(marshaller);
-
-			return webServiceTemplate.marshalSendAndReceive(url, request);
-
-		} catch (SOAPException e) {
-			// TODO Auto-generated catch block
-			System.out.println("ROMPIO SOAPException: " + e.getMessage());
-			e.printStackTrace();
-			return e.getMessage();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("ROMPIO Exception e: " + e.getMessage());
-			return e.getMessage();
-		}
+	public Object callWebService(String url, Object request, String action){
+		return getWebServiceTemplate().marshalSendAndReceive(url, request, new SoapActionCallback(action));
 	}
-
 }
--- PocServiceRest/src/main/java/com/everis/services/rest/Test.java
+++ PocServiceRest/src/main/java/com/everis/services/rest/Test.java
@@ -5,6 +5,7 @@ import javax.xml.ws.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ws.soap.addressing.server.annotation.Action;
 
 import com.everis.connectors.SOAPConnector;
 import com.everis.webservice.afip.Autenticacion;
@@ -17,18 +18,20 @@ public class Test {
 
 	@Autowired
 	Gson g;
+	@Autowired
+	SOAPConnector soapConnector;
 
-	@GetMapping(value = "test.do")
-	public String test(SOAPConnector soapConnector) {
+	@GetMapping(value = "test")
+	public String test() {
 		try {
 
 			ListaEmpresas request = new ListaEmpresas();
 			Autenticacion au = new Autenticacion();
 
 			au.setToken(
-					"PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgc3JjPSJDTj13c2FhaG9tbywgTz1BRklQLCBDPUFSLCBTRVJJQUxOVU1CRVI9Q1VJVCAzMzY5MzQ1MDIzOSIgZHN0PSJvdHJvcyIgdW5pcXVlX2lkPSIzNDMxMTc3NDYyIiBnZW5fdGltZT0iMTUyMzk3NzAwOCIgZXhwX3RpbWU9IjE1MjQwMjAyNjgiLz4KICAgIDxvcGVyYXRpb24gdHlwZT0ibG9naW4iIHZhbHVlPSJncmFudGVkIj4KICAgICAgICA8bG9naW4gZW50aXR5PSIzMzY5MzQ1MDIzOSIgc2VydmljZT0id0dlc1RhYlJlZiIgdWlkPSJTRVJJQUxOVU1CRVI9Q1VJVCAyMDM1Mjk4MDAzMCwgQ049a29jaHBydWViYSIgYXV0aG1ldGhvZD0iY21zIiByZWdtZXRob2Q9IjIyIj4KICAgICAgICAgICAgPHJlbGF0aW9ucz4KICAgICAgICAgICAgICAgIDxyZWxhdGlvbiBrZXk9IjIwMDAwMDAwMDM2IiByZWx0eXBlPSI0Ii8+CiAgICAgICAgICAgICAgICA8cmVsYXRpb24ga2V5PSIyMDM1Mjk4MDAzMCIgcmVsdHlwZT0iNCIvPgogICAgICAgICAgICA8L3JlbGF0aW9ucz4KICAgICAgICA8L2xvZ2luPgogICAgPC9vcGVyYXRpb24+Cjwvc3NvPgo=");
+					"PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgc3JjPSJDTj13c2FhaG9tbywgTz1BRklQLCBDPUFSLCBTRVJJQUxOVU1CRVI9Q1VJVCAzMzY5MzQ1MDIzOSIgZHN0PSJvdHJvcyIgdW5pcXVlX2lkPSIzMzE2NjIzNzk4IiBnZW5fdGltZT0iMTUyNDE1MDA5MCIgZXhwX3RpbWU9IjE1MjQxOTMzNTAiLz4KICAgIDxvcGVyYXRpb24gdHlwZT0ibG9naW4iIHZhbHVlPSJncmFudGVkIj4KICAgICAgICA8bG9naW4gZW50aXR5PSIzMzY5MzQ1MDIzOSIgc2VydmljZT0id0dlc1RhYlJlZiIgdWlkPSJTRVJJQUxOVU1CRVI9Q1VJVCAyMDM1Mjk4MDAzMCwgQ049a29jaHBydWViYSIgYXV0aG1ldGhvZD0iY21zIiByZWdtZXRob2Q9IjIyIj4KICAgICAgICAgICAgPHJlbGF0aW9ucz4KICAgICAgICAgICAgICAgIDxyZWxhdGlvbiBrZXk9IjIwMDAwMDAwMDM2IiByZWx0eXBlPSI0Ii8+CiAgICAgICAgICAgICAgICA8cmVsYXRpb24ga2V5PSIyMDM1Mjk4MDAzMCIgcmVsdHlwZT0iNCIvPgogICAgICAgICAgICA8L3JlbGF0aW9ucz4KICAgICAgICA8L2xvZ2luPgogICAgPC9vcGVyYXRpb24+Cjwvc3NvPgo=");
 			au.setSign(
-					"TiSOTvebaeFD/u001KZevY2fUws2hlvD+4dZ5az1cDmvytGVV6Or30L3QEhl7G2Jw9318pSn3QNFXAJW29ik/pVkSvlRWH+V2XXba79QzSkrqrzHOiEUKJEuHgGIzC+QNp9df0mscnBodADCxaV3L5PRJ4snqG4sS4WFt5gk930=");
+					"c6cP7N3Cr7ThQc2J0nca3MnO5M388sj1CJGpeVQTcCEs9A+zX2CRB9ICMLbY7eEdWAFBHxYN6L/Dd5ySHsM444nGKeOPoS86LDvna94E2RY3ZXfVI7eCyA7lo4VvScBeqy7doez9QD524nD6Ehj4rK0NkurSFzZbCVLCmqTr4rk=");
 			au.setCuit("20000000036");
 			au.setRol("EXTE");
 			au.setTipoAgente("IMEX");
@@ -37,10 +40,11 @@ public class Test {
 			request.setIdReferencia("EMP_TRAN");
 
 			ListaEmpresasResponse response = (ListaEmpresasResponse) soapConnector
-					.callWebService("https://testdia.afip.gob.ar/Dia/ws/wGesTabRef/wGesTabRef.asmx?wsdl", request);
+					.callWebService("https://testdia.afip.gob.ar/Dia/ws/wGesTabRef/wGesTabRef.asmx", request, "ar.gov.afip.dia.serviciosweb.wgesTabRef/ListaEmpresas");
 //			g.toJson(response.getListaEmpresasResult());
-			return response.getListaEmpresasResult().getEmpresas().toString();
+			return g.toJson(response.getListaEmpresasResult()); //response.getListaEmpresasResult().getEmpresas().toString();
 		} catch (Exception e) {
+			e.printStackTrace();
 			return e.getMessage();
 		}
 	}
